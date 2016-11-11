3a5950268ed37e58dd47142b622fdde032efee8e
satyanarayan <satyanarayan.gullapalli@mindtree.com>
2016-11-11 12:06:18 +0530

    scrolling display started

---
+++ projects/scrolling_display/include/scroll_data.h
@@ -0,0 +1,8 @@
+#ifndef __scroll_data_h__
+#define __scroll_data_h__
+
+#include <datatypes.h>
+
+void ScrollMessage (s8 *message);
+
+#endif
---
+++ projects/scrolling_display/src/main.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+#include <scroll_data.h>
+
+
+int main (void) {
+    
+    ScrollMessage ("A");
+    
+    return 0;
+}
