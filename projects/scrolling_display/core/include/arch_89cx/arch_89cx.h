56ef812cd71d78eeab0e93d48cefd901f8f75966
Satyagp49 <satya.temp@gmail.com>
2016-11-15 01:54:20 +0530

    uart started still to complete

--- projects/scrolling_display/core/include/arch_89cx/arch_89cx.h
+++ projects/scrolling_display/core/include/arch_89cx/arch_89cx.h
@@ -5,7 +5,7 @@
 #include <timer_89cx.h>
 #include <REG51_89EV516RD.H>
 
-sbit LED = P3^3;
+sbit LED_INDICATION = P3^3;
 
 #define LED_MESSAGE_LEN 48
 
@@ -15,6 +15,8 @@ sbit LED = P3^3;
 
 void controller_init_89cx (void);
 void peripheral_init_89cx (void);
+u16 uart_get_str (u8 *msg_str, u16 len);
+u8 uart_get_char (void);
 
 void LedSendMessage_89cx (u8 led_msg);
 void LedMoveToNextLine_89cx (void);
--- projects/scrolling_display/core/src/core_sys_init.c
+++ projects/scrolling_display/core/src/core_sys_init.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <led_message_scrolling.h>
 #include <arch_xyz.h>
+#include <string.h>
 
 void timer_isr_testing (void);
 
 void main (void) {
-        
+    u8 msg_str = 0x00;    
     controller_init ();
     
     peripheral_init ();
@@ -17,28 +18,35 @@ void main (void) {
     //UpdateScrollMessage ("ABC");
     
     // timer delay testing
-    while (1)
+    /*while (1)
     {
         DelayMs (50);
-        LED = 0x00;
+        LED_INDICATION = 0x00;
         DelayMs (200);
-        LED = 0x01;
-    }             
+        LED_INDICATION = 0x01;
+    }*/             
     
+    while (1)
+    {
+        msg_str = uart_get_char ();
+		if ((msg_str != 0) && (msg_str != 0xFF)) {
+			printf ("%c\r", msg_str);
+		}
+    }
     printf ("\n");
 }
 
 void toggle_led (void) {
-    if (LED == 0) {
-        LED = 1;
+    if (LED_INDICATION == 0) {
+        LED_INDICATION = 1;
     }
     else {
-        LED = 0;
+        LED_INDICATION = 0;
     }
 }
 void timer_isr_testing () {
     TIMER_CONFIG timer_config;
-    LED = 0;
+    LED_INDICATION = 0;
     timer_config.timer = TIMER_0;
     timer_config.time_out = 50;
     timer_config.isr_handler = toggle_led;
