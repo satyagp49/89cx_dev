e8f44025dce03055dfa9a80692df990c9e67d2f5
Satyagp49 <satya.temp@gmail.com>
2016-11-17 20:28:03 +0530

    scrolling started but direction to be changed

--- projects/scrolling_display/proteus/8x8 LED_code/ScrollLED - Copy.c
+++ projects/scrolling_display/proteus/8x8 LED_code/ScrollLED - Copy.c
@@ -30,11 +30,11 @@ void send_data(unsigned int temp){
   ST_Clk = 0;
 
 }
-//unsigned short dummyCharData [8] = { 0x0C, 0x1E, 0x33, 0x33, 0x3F, 0x33, 0x33, 0x00};//{0x0E, 0x11, 0x11, 0x11, 0x1F, 0x11, 0x11, 0x11};//{0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11};
+
 unsigned int DisplayBuffer[]={0,0,0,0,0,0,0,0};
 unsigned int speed;
 short  l, k, ShiftAmount, scroll, temp, shift_step=1, StringLength;
-char message[]="ABCD123";
+char message[]="AB";
 char index;
 void main() {
     unsigned char select_line = 0x00, count;
@@ -44,16 +44,17 @@ void main() {
 			for (scroll=0; scroll<(8/shift_step); scroll++) {
 				for (ShiftAmount=0; ShiftAmount<8; ShiftAmount++){
 					index = message[k];
-					temp = CharData[index][ShiftAmount];
+					temp = CharData[index-32][ShiftAmount];
 					DisplayBuffer[ShiftAmount] = (DisplayBuffer[ShiftAmount] << shift_step)| (temp >> ((8-shift_step)-scroll*shift_step));
 				}
+				
 				speed = 1;
 				for(l=0; l<speed;l++){
 					for (count=0; count<8; count++) {
 						send_data(DisplayBuffer[count]);
 						select_line = (0xFF & ~(0x01 << count));
 						P3 = select_line;
-						delay_ms(20);
+						delay_ms(10);
 					}  // i
 				} // l
 			} // scroll
