7a607f049ba60832b36dbe0ef65aade2c693bf5c
satyanarayan <satyanarayan.gullapalli@mindtree.com>
2016-11-17 16:42:48 +0530

    still need to fix display data

--- projects/scrolling_display/proteus/8x8 LED_code/main.c
+++ projects/scrolling_display/proteus/8x8 LED_code/main.c
@@ -6,9 +6,6 @@ sbit DS = P0^1;
 sbit SH_CP = P0^0;
 sbit ST_CP = P0^2;
 
-sbit NEXT_LINE = P3^0;
-sbit RESET_LINE = P3^1;
-
 
 void delay_ms(unsigned int x)	 // delays x msec (at fosc=11.0592MHz)
 {
@@ -25,20 +22,30 @@ void display_data(unsigned char temp){
    if(Flag==0) DS = 0;
    else DS = 1;
    SH_CP = 1;
-   delay_ms(100);
    SH_CP = 0;
    Mask = Mask << 1;
-
   }
   // Apply clock on ST_Clk
   ST_CP = 1;
-  delay_ms(100);
   ST_CP = 0;
-
+  //delay_ms(200);
 }
 void main()
 {   
-    unsigned char count = 0x00, count1 = 0x00, flag = 0x00;
+    unsigned char count = 0x00, select_line = 0xFE, flag = 0x00;
+    unsigned char ascii_data [] = {0x0E, 0x11, 0x11, 0x11, 0x1F, 0x11, 0x11, 0x11};
+    display_data (0x11);
+	while(1)
+	{
+        select_line = 0xFE;
+        for (count = 0x00; count < 8; count ++) {		
+            display_data (ascii_data [count]);
+            P3 = select_line;
+            delay_ms(10);
+            select_line <<= 1;
+        }
+
+	} 
      //test counter
     /*RESET_LINE = 1;
     RESET_LINE = 0;
@@ -48,14 +55,6 @@ void main()
         delay_ms(100);                   
     }*/
 
-    unsigned char ascii_data [] = {0x0E, 0x11, 0x11, 0x11, 0x1F, 0x11, 0x11, 0x11};
-	while(1)
-	{
-        for (count = 0x00; count < 8; count ++) {		
-            display_data (ascii_data [count]);
-            delay_ms(10);
-        }
-	} 
     /*
     while (1) {
         ST_CP = 1;
@@ -85,6 +84,4 @@ void main()
         RESET_LINE = 1;        
         RESET_LINE = 0;
     }*/	
-
-
 }
